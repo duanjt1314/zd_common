846a77f0f74bb1293e779fcecad2d9576b2e9a47
duanjt1314 <479224006@qq.com>
2017-07-17 09:59:32 +0800

    增加线程基础类ThreadBase，并提供一些常用的方法

---
+++ src/main/java/cn/zdsoft/common/base/ThreadBase.java
@@ -0,0 +1,30 @@
+package cn.zdsoft.common.base;
+
+/**
+ * 线程基类，提供常用的实现方法等
+ * @author 段江涛
+ * @date 2017-07-17
+ */
+public abstract class ThreadBase extends Thread {
+	/**
+	 * 是否正在运行
+	 */
+	protected boolean running;
+
+	public abstract void Start();
+
+	public abstract void Stop();
+
+	/**
+	 * 程序暂停多少秒
+	 * @param seconds 需要等待的时间(单位:秒)
+	 * @throws InterruptedException 
+	 */
+	protected void Wait(int seconds) throws InterruptedException {
+		int i = 0;
+		while (i < seconds && running) {
+			Thread.sleep(1000);
+			i++;
+		}
+	}
+}
