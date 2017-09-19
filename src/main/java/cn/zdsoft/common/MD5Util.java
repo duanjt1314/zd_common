16c10cb0eee0b8283015d144082df1c506663407
duanjt1314 <479224006@qq.com>
2017-09-19 13:54:56 +0800

    增加MD5加密

---
+++ src/main/java/cn/zdsoft/common/MD5Util.java
@@ -0,0 +1,39 @@
+package cn.zdsoft.common;
+
+import java.security.MessageDigest;
+
+/**
+ * MD5加密
+ * @author 段江涛
+ * @date 2017-09-19
+ */
+public class MD5Util {
+	/**
+	 * 对内容进行MD5加密
+	 * @param content
+	 * @return
+	 */
+	public static String Encrypt(String content) {
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+			byte[] byteArray = content.getBytes("UTF-8");
+			byte[] md5Bytes = md5.digest(byteArray);
+			StringBuffer hexValue = new StringBuffer();
+			for (int i = 0; i < md5Bytes.length; i++) {
+				int val = ((int) md5Bytes[i]) & 0xff;
+				if (val < 16) {
+					hexValue.append("0");
+				}
+				hexValue.append(Integer.toHexString(val));
+			}
+			String result = hexValue.toString();
+			result = result.substring(0, result.length() - 2).toUpperCase();
+			return result;
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			e.printStackTrace();
+			return "";
+		}
+	}
+}
---
+++ src/test/java/cn/zdsoft/common/RC4EncryptTest.java
@@ -0,0 +1,45 @@
+package cn.zdsoft.common;
+
+import static org.junit.Assert.*;
+
+import java.util.Base64;
+
+import org.junit.Test;
+
+import sun.misc.BASE64Encoder;
+
+public class RC4EncryptTest {
+
+	/**
+	 * 解密
+	 * @throws Exception 
+	 */
+	@Test
+	public void testDecrypt() throws Exception {
+//		String content="我是中国人";
+//		byte[] bs=content.getBytes("utf-8");
+//		bs=RC4Encrypt.encrypt(bs, "hzsoft|hzsoft|hzsoft|hzsoft|hzsoft|hzsoft|hzsoft|hzsoft|hzsoft");
+//		System.out.println(Base64.getEncoder().encodeToString(bs));
+		
+		String content="我是中国人";
+		String res= Base64.getEncoder().encodeToString(content.getBytes("utf-8"));
+		System.out.println(new BASE64Encoder().encode(content.getBytes("utf8")));
+		System.out.println(res);
+		
+	}
+
+	/**
+	 * 加密
+	 * @throws Exception 
+	 */
+	@Test
+	public void testEncrypt() throws Exception {
+		String content="我是中国人";
+		byte[] bs=content.getBytes("utf-8");
+		bs=RC4Encrypt.decrypt(bs, "hzsoft|hzsoft|hzsoft");
+		
+		bs=RC4Encrypt.decrypt(bs, "hzsoft|hzsoft|hzsoft");
+		System.out.println(new String(bs));
+	}
+
+}
