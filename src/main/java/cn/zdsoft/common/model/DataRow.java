1350d5ce3e130b88bed5a3a87a5a03c37613e093
duanjt1314 <479224006@qq.com>
2018-08-15 16:46:16 +0800

    DataRow使用LinkedCaseInsensitiveMap，有序的Map集合

--- src/main/java/cn/zdsoft/common/model/DataRow.java
+++ src/main/java/cn/zdsoft/common/model/DataRow.java
@@ -1,13 +1,13 @@
 package cn.zdsoft.common.model;
 
-import org.apache.commons.collections.map.CaseInsensitiveMap;
+import org.springframework.util.LinkedCaseInsensitiveMap;
 
 /**
  * 数据行
  * @author Administrator
  * HashMap是无序的，LinkedHashMap是有序的(取数据时先插入先取出)
  */
-public class DataRow extends CaseInsensitiveMap{
+public class DataRow extends LinkedCaseInsensitiveMap{
 
 	public Object put(String key, Object value) {
 		return super.put(key, value);
---
+++ src/test/java/cn/zdsoft/common/model/DataRowTest.java
@@ -0,0 +1,14 @@
+package cn.zdsoft.common.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataRowTest {
+
+	@Test
+	public void test() {
+				
+	}
+
+}
